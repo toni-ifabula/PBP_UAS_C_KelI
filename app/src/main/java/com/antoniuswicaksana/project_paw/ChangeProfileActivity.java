7aa6204d2c50987bb9be0b8dbabb050b18acd2e3
toni612 <toni61200@gmail.com>
2020-09-28 20:32:27 +0700

    first commit

---
+++ app/src/main/java/com/antoniuswicaksana/project_paw/ChangeProfileActivity.java
@@ -0,0 +1,38 @@
+package com.antoniuswicaksana.project_paw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ChangeProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.menu_main){
+            startActivity(new Intent(this, MainActivity.class));
+        } else if (item.getItemId() == R.id.menu_profile) {
+            startActivity(new Intent(this, ProfileActivity.class));
+        }
+
+        return true;
+    }
+}
