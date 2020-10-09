8cb8fd97167c74643fc7840ac5febc98332dbc41
toni612 <toni61200@gmail.com>
2020-10-09 18:56:10 +0700

    add profile sharedPreferences

--- app/src/main/java/com/antoniuswicaksana/project_pbp/ChangeProfileActivity.java
+++ app/src/main/java/com/antoniuswicaksana/project_pbp/ChangeProfileActivity.java
@@ -1,10 +1,12 @@
 package com.antoniuswicaksana.project_pbp;
 
+import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,22 +14,39 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class ChangeProfileActivity extends AppCompatActivity {
 
     Button btnSumbit, btnCancel;
+    TextInputEditText inputNama, inputAlamat, inputNoTelp;
     private static String CHANNEL_ID = "Channel 1";
+    private SharedPreferences preferences;
+    public static final int mode = Activity.MODE_PRIVATE;
+    private String nama = "";
+    private String alamat = "";
+    private String noTelp = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+        loadPreferences();
 
+        inputNama = findViewById(R.id.inputNama);
+        inputAlamat = findViewById(R.id.inputAlamat);
+        inputNoTelp = findViewById(R.id.inputNoTelp);
         btnSumbit = findViewById(R.id.btnSubmit);
         btnCancel = findViewById(R.id.btnCancel);
+
+        inputNama.setText(nama);
+        inputAlamat.setText(alamat);
+        inputNoTelp.setText(noTelp);
 
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +60,7 @@ public class ChangeProfileActivity extends AppCompatActivity {
         btnSumbit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                savePreferences();
                 createNotificationChannel();
                 addNotification();
                 Intent intent = new Intent(ChangeProfileActivity.this,ProfileActivity.class);
@@ -70,6 +90,30 @@ public class ChangeProfileActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    private void loadPreferences() {
+        String name = "profile";
+        preferences = getSharedPreferences(name, mode);
+        if (preferences!=null) {
+            nama = preferences.getString("nama", "");
+            alamat = preferences.getString("alamat", "");
+            noTelp = preferences.getString("noTelp", "");
+        }
+    }
+
+    private void savePreferences() {
+        SharedPreferences.Editor editor = preferences.edit();
+        if (!inputNama.getText().toString().isEmpty() && !inputAlamat.getText().toString().isEmpty() &&
+                !inputNoTelp.getText().toString().isEmpty()) {
+            editor.putString("nama", inputNama.getText().toString());
+            editor.putString("alamat", inputAlamat.getText().toString());
+            editor.putString("noTelp", inputNoTelp.getText().toString());
+            editor.apply();
+            Toast.makeText(this, "Profile saved", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Fill correctly", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void createNotificationChannel () {
--- app/src/main/java/com/antoniuswicaksana/project_pbp/ProfileActivity.java
+++ app/src/main/java/com/antoniuswicaksana/project_pbp/ProfileActivity.java
@@ -1,25 +1,43 @@
 package com.antoniuswicaksana.project_pbp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
     Button btn_ganti_profil;
+    TextView txtNama, txtAlamat, txtNoTelp;
+    ChangeProfileActivity changeProfileClass;
+    private SharedPreferences preferences;
+    public static final int mode = Activity.MODE_PRIVATE;
+    private String nama = "";
+    private String alamat = "";
+    private String noTelp = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        loadPreferences();
 
+        txtNama = findViewById(R.id.txtNama);
+        txtAlamat = findViewById(R.id.txtAlamat);
+        txtNoTelp = findViewById(R.id.txtNoTelp);
         btn_ganti_profil = findViewById(R.id.btn_ganti_profil);
+
+        txtNama.setText(nama);
+        txtAlamat.setText(alamat);
+        txtNoTelp.setText(noTelp);
 
         btn_ganti_profil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,5 +67,15 @@ public class ProfileActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    private void loadPreferences() {
+        String name = "profile";
+        preferences = getSharedPreferences(name, mode);
+        if (preferences!=null) {
+            nama = preferences.getString("nama", "");
+            alamat = preferences.getString("alamat", "");
+            noTelp = preferences.getString("noTelp", "");
+        }
     }
 }
